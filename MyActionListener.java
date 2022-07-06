4e85b206ed96699175f2a995e0fee4f46acb185f
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-07-05 17:10:09 -0700

    Why is this so slow

--- MyActionListener.java
+++ MyActionListener.java
@@ -3,7 +3,7 @@ import java.awt.event.ActionEvent;
 
 public class MyActionListener implements ActionListener{
 
-    public static final int DELAY = 1;
+    public static final int DELAY = 10;
     public MyPanel panel;
 
     public MyActionListener(MyPanel panel){
